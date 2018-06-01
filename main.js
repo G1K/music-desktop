60fb74e76fb549ac1c5f4dd48ac4d4f1bb92b995
mrG1K <mr@g1k.ru>
2018-06-01 23:03:25 +0300

    Add shortcut key

--- main.js
+++ main.js
@@ -1,54 +1,63 @@
 const path = require('path');
 const tray = require('./tray');
 
-const { app,
-        BrowserWindow,
-        globalShortcut } = require('electron');
-const { embedTouchBar } = require('./touchbar.js');
-const config = require('./config/config.js');
-const { showNotifications } = require('./notifications/notificatoins.js');
+const {
+		  app,
+		  BrowserWindow,
+		  globalShortcut
+	  }               = require('electron');
+const {embedTouchBar} = require('./touchbar.js');
 
+app.on('ready', function () {
+	const mainWindow = new BrowserWindow({
+		width: 1024,
+		height: 768,
+		title: 'Yandex.Music',
+		icon: path.join(__dirname, 'icon.png'),
+		webPreferences: {
+			nodeIntegration: false,
+			preload: path.join(__dirname, 'preload.js')
+		}
+	});
+	app.isQuiting    = false;
 
-app.on('ready', function() {
-    const hideMenuBar = config.get("hideMenuBar", false);
-    const mainWindow = new BrowserWindow({
-        width: 1024,
-        height: 768,
-        title: 'Yandex.Music',
-        autoHideMenuBar: hideMenuBar,
-        icon: path.join(app.getAppPath(), 'static/icon.png'),
-        webPreferences: {
-            nodeIntegration: false,
-            preload: path.join(__dirname, 'preload.js'),
-        },
-    });
-    require("./menu/mainmenu.js");
+	mainWindow.on('close', function (e) {
+		if (!app.isQuiting) {
+			e.preventDefault();
+			if (process.platform === 'darwin') {
+				app.hide();
+			} else {
+				mainWindow.hide();
+			}
+		}
+		return false;
+	});
+	mainWindow.on('minimize', function (e) {
+		e.preventDefault();
+		if (process.platform === 'darwin') {
+			app.hide();
+		} else {
+			mainWindow.hide();
+		}
+	});
 
-    mainWindow.on('closed', function() {
-        app.quit();
-    });
 
 	tray.create(mainWindow);
 
-    mainWindow.loadURL('https://music.yandex.ru');
+	mainWindow.loadURL('https://music.yandex.ru');
+	embedTouchBar(mainWindow);
 
-    showNotifications();
+	globalShortcut.register('mediaplaypause', function () {
+		mainWindow.webContents.send('playpause');
+	});
 
-    if (process.platform === 'darwin') {
-        embedTouchBar(mainWindow);
-    }
+	globalShortcut.register('medianexttrack', function () {
+		mainWindow.webContents.send('next');
+	});
 
-    globalShortcut.register('mediaplaypause', function() {
-        mainWindow.webContents.send('playpause');
-    });
-
-    globalShortcut.register('medianexttrack', function() {
-        mainWindow.webContents.send('next');
-    });
-
-    globalShortcut.register('mediaprevioustrack', function() {
-        mainWindow.webContents.send('prev');
-    });
+	globalShortcut.register('mediaprevioustrack', function () {
+		mainWindow.webContents.send('prev');
+	});
 
 	globalShortcut.register('Super+Alt+Right', () => {
 		mainWindow.webContents.send('next');
